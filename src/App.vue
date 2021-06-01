5dea38c9950317b2d3a1cbbe7e76ea43fc69df68
Florian Berrotµ <florian.berrot@ynov.com>
2021-06-01 12:59:32 +0200

    feat: manifest is detect on browser

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <link rel="manifest" href="/manifest.json" crossorigin="use-credentials">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
