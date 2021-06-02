f43129141e0a69d390e2801e47a73d9818a1f18a
Sanchez <gregg.sanchez.pro@gmail.com>
2021-06-02 15:52:26 +0200

    style (views) : add scss file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+// CSS / SCSS
+import './assets/scss/main.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
