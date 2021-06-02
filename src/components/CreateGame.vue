f8299c1177996802a7eff0e59ef68433c8ca9c98
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2021-06-02 17:36:19 +0200

    feat: add validate function to valid form

--- src/components/CreateGame.vue
+++ src/components/CreateGame.vue
@@ -4,30 +4,77 @@
       <p>Création d'une nouvelle partie</p>
       <v-divider></v-divider>
     </v-col>
-    <!-- New game name -->
-    <v-col xs="12">
-      <p>Nom de la partie</p>
-      <v-text-field
-          label="Nom de la partie"
-          outlined
-          clearable
-      ></v-text-field>
-    </v-col>
-    <!-- Max player -->
-<!--    <v-col xs="12">-->
-<!--      <p>Nombre de joueurs maximum</p>-->
-<!--      <vue-number-input v-model="value" :min="1" :max="10" inline controls></vue-number-input>-->
-<!--    </v-col>-->
-    <!-- Map choice -->
-    <v-col xs="12">
-      <p>Choix de la map</p>
-      <v-select
-          :items="itemsMap"
-          label="Choix de la map"
-          outlined
-      ></v-select>
-      <v-divider></v-divider>
-    </v-col>
+
+    <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+    >
+      <!-- New game name -->
+      <v-col xs="12">
+        <p>Nom de la partie</p>
+        <v-text-field
+            label="Nom de la partie"
+            outlined
+            clearable
+            v-model="nameGame"
+            :counter="15"
+            :rules="nameGameRules"
+            required
+        ></v-text-field>
+      </v-col>
+      <!-- Max player -->
+  <!--    <v-col xs="12">-->
+  <!--      <p>Nombre de joueurs maximum</p>-->
+  <!--      <vue-number-input v-model="value" :min="1" :max="10" inline controls></vue-number-input>-->
+  <!--    </v-col>-->
+      <!-- Map choice -->
+      <v-col xs="12">
+        <p>Choix de la map</p>
+        <v-select
+            :items="itemsMap"
+            label="Choix de la map"
+            outlined
+        ></v-select>
+        <v-divider></v-divider>
+      </v-col>
+      <!-- Private game -->
+      <v-col xs="12">
+        <v-checkbox
+            v-model="privateGame"
+            label="Partie privé"
+        ></v-checkbox>
+        <!-- Code -->
+        <v-expand-transition>
+          <div v-show="privateGame">
+            <p>Code d'invitation</p>
+            <v-text-field
+                label="code"
+                placeholder="zk88bfz6jb9"
+                outlined
+                clearable
+                v-if="privateGame"
+                v-model="codeJoinGame"
+                :counter="10"
+                :rules="codeJoinGameRules"
+                required
+            ></v-text-field>
+          </div>
+        </v-expand-transition>
+      </v-col>
+      <!-- Actions buttons -->
+      <v-col xs="12">
+        <v-btn
+            elevation="2"
+        >Annuler</v-btn>
+        <v-btn
+            elevation="2"
+            color="error"
+            :disabled="!valid"
+            @click="validate"
+        >Créer la partie</v-btn>
+      </v-col>
+    </v-form>
 
   </div>
 </template>
@@ -37,9 +84,29 @@
     name: "CreateGame",
     data() {
       return {
-        itemsMap: ["Map #1", "Map #2", "Map #3"]
+        itemsMap: ["Map #1", "Map #2", "Map #3"],
+        privateGame: false,
+        valid: true,
+        nameGame: '',
+        nameGameRules: [
+          v => !!v || 'Nom de la partie obligatoire',
+          v => (v && v.length <= 15) || 'Le nom ne doit pas depasser 15 caractères',
+        ],
+        codeJoinGame: '',
+        codeJoinGameRules: [
+          v => !!v || 'Code de la partie obligatoire',
+          v => (v && v.length <= 10) || 'Le code ne doit pas depasser 10 caractères',
+        ],
       };
     },
+    methods: {
+      validate () {
+        // console.log("Private game " + this.privateGame)
+        // console.log("Code value " + this.codeJoinGame)
+        // console.log("Form " + this.$refs.form.validate())
+        // this.$refs.form.validate()
+      },
+    }
   }
 </script>
 
