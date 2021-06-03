04745b68a8d04e1ce6d99f0b4b55adab0ea2f119
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2021-06-03 10:27:41 +0200

    feat: Add snackbar message

--- src/components/CreateGame.vue
+++ src/components/CreateGame.vue
@@ -91,6 +91,20 @@
       </v-col>
     </v-form>
     <!-- End create new game form -->
+
+    <v-snackbar v-model="snackbar" :timeout="timeoutSnackbar" :color="colorMessage">
+      {{ textMessageValidForm }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="white"
+            text
+            v-bind="attrs"
+            @click="snackbar = false"
+        >
+          Fermer
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
@@ -102,6 +116,10 @@
         itemsMap: ["Map #1", "Map #2", "Map #3"],
         privateGame: false,
         valid: true,
+        snackbar: false,
+        colorMessage: '',
+        textMessageValidForm: '',
+        timeoutSnackbar: 4000,
         // Name part rules
         nameGame: '',
         nameGameRules: [
@@ -132,13 +150,18 @@
     methods: {
       // Validate form
       validate () {
+        this.snackbar = true
         // console.log("Private game " + this.privateGame)
         // console.log("Code value " + this.codeJoinGame)
         // console.log("Form = " + this.$refs.form.validate())
         if(this.$refs.form.validate()) {
           console.log("Form OK")
+          this.textMessageValidForm = "Votre partie à bien été créée"
+          this.colorMessage = "green lighten-2";
         } else {
           console.log("Form KO")
+          this.textMessageValidForm = "Des champs sont incorrectes"
+          this.colorMessage = "red lighten-2";
         }
       },
       //  Reset form
