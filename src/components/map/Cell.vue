9197b9636e5c5175f08cbb2b973b1a5f17853f9a
Gregg Sanchez <gregg.sanchez.pro@gmail.com>
2021-06-05 13:58:53 +0200

    feat (map) : indent & import file format

--- src/components/map/Cell.vue
+++ src/components/map/Cell.vue
@@ -1,28 +1,23 @@
 <template>
-  <div class="cell" :style="styleCell" @click="move">
-    <div class="background" :style="styleBackground"></div>
-    <div class="obstacle">
-        <slot name="obstacle"></slot>
+    <div class="cell" :style="styleCell" @click="move">
+        <div class="background" :style="styleBackground"></div>
+        <div class="obstacle">
+            <slot name="obstacle"></slot>
+        </div>
+        <div class="decoration">
+            <slot name="decoration"></slot>
+        </div>
+        <div class="caracter" v-if="player">
+            <v-tooltip left>
+                <template v-slot:activator="{ on, attrs }">
+                    <img :src="player.vitality > 0 ? player.img : ripImage" alt="caracter"
+                         v-bind="attrs" v-on="on"/>
+                </template>
+                <span>{{ player.username }}</span>
+            </v-tooltip>
+        </div>
+        <div class="highlight" :class="(obstacleTile !== -1 && decorationTile === -1) ? 'red_obstacle' : (isAccessible && !player) ? 'accessible' : null"></div>
     </div>
-    <div class="decoration">
-      <slot name="decoration"></slot>
-    </div>
-    <div class="caracter" v-if="player">
-      <v-tooltip left>
-        <template v-slot:activator="{ on, attrs }">
-          <img :src="player.vitality > 0 ? player.img : ripImage" alt="caracter" 
-            v-bind="attrs" v-on="on"/>
-        </template>
-        <span>{{ player.username }}</span>
-      </v-tooltip>
-    </div>
-    <div class="highlight"
-      :class="
-        (obstacleTile !== -1 && decorationTile === -1) ? 'red_obstacle'
-        : (isAccessible && !player) ? 'accessible' : null
-      "
-    ></div>
-  </div>
 </template>
 
 <script>
@@ -33,54 +28,54 @@ const tileSize = 32
 const tileSetWidth = 19
 
 export default {
-  name: 'Cell',
-  props: {
-    x: Number,
-    y: Number,
-    tileNumber: Number,
-    obstacleTile: Number,
-    decorationTile: Number,
-    isAccessible: Boolean,
-    player: Object,
-  },
-  data() {
-    return {
-      styleCell: {
-        width: `${tileSize}px`,
-        height: `${tileSize}px`,
-        minWidth: `${tileSize}px`,
-        minHeight: `${tileSize}px`,
-      },
-      styleBackground: {},
-      bgImage: bgImage,
-      ripImage: ripImage,
-    }
-  },
-  mounted() {
-    this.setup()
-  },
-  methods: {
-    setup() {
-      const coordinates = this.getTileCoordinates();
-      this.styleBackground = {
-        backgroundPosition: `-${coordinates.x * tileSize}px -${coordinates.y * tileSize}px`,
-        height: '100%',
-        width: '100%',
-        backgroundImage: `url(${this.bgImage})`
-      }
+    name: 'Cell',
+    props: {
+        x: Number,
+        y: Number,
+        tileNumber: Number,
+        obstacleTile: Number,
+        decorationTile: Number,
+        isAccessible: Boolean,
+        player: Object,
     },
-    getTileCoordinates(){
-      return {
-        x : this.tileNumber % tileSetWidth,
-        y : Math.floor(this.tileNumber / tileSetWidth)
-      }
+    data() {
+        return {
+            styleCell: {
+                width: `${tileSize}px`,
+                height: `${tileSize}px`,
+                minWidth: `${tileSize}px`,
+                minHeight: `${tileSize}px`,
+            },
+            styleBackground: {},
+            bgImage: bgImage,
+            ripImage: ripImage,
+        }
     },
-    move() {
-      if(this.isAccessible) {
-        this.$emit('move', { x: this.x, y: this.y })
-      }
+    mounted() {
+        this.setup()
+    },
+    methods: {
+        setup() {
+            const coordinates = this.getTileCoordinates();
+            this.styleBackground = {
+                backgroundPosition: `-${coordinates.x * tileSize}px -${coordinates.y * tileSize}px`,
+                height: '100%',
+                width: '100%',
+                backgroundImage: `url(${this.bgImage})`
+            }
+        },
+        getTileCoordinates() {
+            return {
+                x: this.tileNumber % tileSetWidth,
+                y: Math.floor(this.tileNumber / tileSetWidth)
+            }
+        },
+        move() {
+            if (this.isAccessible) {
+                this.$emit('move', {x: this.x, y: this.y})
+            }
+        }
     }
-  }
 }
 </script>
 
--- src/components/map/Grid.vue
+++ src/components/map/Grid.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="grid">
-    <div class="grid-container">
-      <div class="grid-row" v-for="(row, i) in rows" :key="i">
-        <cell v-for="(cell, j) in row" :key="j" @move="(e) => playerMove(e)"
-              :x="cell.x" :y="cell.y" :tileNumber="cell.backgroundTile" :obstacleTile="cell.obstacleTile"
-              :decorationTile="cell.decorationTile" :isAccessible="cell.isAccessible" :player="cell.player">
-          <cell v-if="cell.obstacleTile !== -1" slot="obstacle" :tileNumber="cell.obstacleTile"></cell>
-          <cell v-if="cell.decorationTile !== -1" slot="decoration" :tileNumber="cell.decorationTile"></cell>
-        </cell>
-      </div>
+    <div class="grid">
+        <div class="grid-container">
+            <div class="grid-row" v-for="(row, i) in rows" :key="i">
+                <cell v-for="(cell, j) in row" :key="j" @move="(e) => playerMove(e)"
+                      :x="cell.x" :y="cell.y" :tileNumber="cell.backgroundTile" :obstacleTile="cell.obstacleTile"
+                      :decorationTile="cell.decorationTile" :isAccessible="cell.isAccessible" :player="cell.player">
+                    <cell v-if="cell.obstacleTile !== -1" slot="obstacle" :tileNumber="cell.obstacleTile"></cell>
+                    <cell v-if="cell.decorationTile !== -1" slot="decoration" :tileNumber="cell.decorationTile"></cell>
+                </cell>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -18,128 +18,130 @@ import dataMap from '../../assets/json/village.json';
 import Cell from './Cell';
 
 export default {
-  name: 'Grid',
-  components: {
-    Cell
-  },
-  props: {
-    players: Array,
-    currentPlayer: Object,
-  },
-  data() {
-    return {
-      rows: [],
-      moves: 0,
-    }
-  },
-  methods: {
-    setupObstacles(obstacleProbability) {
-      this.rows.map(row => {
-        row.map(cell => {
-          const isObstacle = Math.random() < obstacleProbability
-          if (isObstacle) {
-            //console.log(cell)
-          }
-          if (cell.obstacleTile !== -1) {
-            //cell.backgroundTile = cell.obstacleTile
-          }
-          //cell.obstacleTile = isObstacle ? 285 : -1
-        })
-      })
+    name: 'Grid',
+    components: {
+        Cell
     },
-    createGridFromJSON(data) {
-      console.log(data);
-      const width = data.width; // Map width
-      let currentRow = []; // Contains our future cells
-      for (let i = 0; i < data.layers[0].data.length; i++) {
-        if (i % width === 0 && i !== 0) {
-          this.rows.push(currentRow);
-          currentRow = [];
+    props: {
+        players: Array,
+        currentPlayer: Object,
+    },
+    data() {
+        return {
+            rows: [],
+            moves: 0,
         }
-        // Push our cell inside our current row
-        currentRow.push({
-          x: i % width,
-          y: Math.floor(i / width),
-          backgroundTile: data.layers[0].data[i] - 1,
-          obstacleTile: data.layers[1].data[i] - 1,
-          decorationTile: data.layers[2].data[i] - 1,
-        });
-      }
-      this.rows.push(currentRow)
     },
-    setupCurrentPlayer() {
-      console.log(this.players)
-      this.setAccessibleCellsAroundPlayer(this.currentPlayer.coordinates.x, this.currentPlayer.coordinates.y)
-    },
-    placePlayersOnMap() {
-      this.players.map(player => {
-        this.rows[player.coordinates.y][player.coordinates.x].player = player
-      })
-    },
-    isWalkableCell({ x, y }) {
-      if(x < 0 || x >= this.rows[0].length || y < 0 || y >= this.rows.length) {
-        return false
-      }
-      return ((this.rows[y][x].obstacleTile === -1 || this.rows[y][x].decorationTile !== -1) && !this.rows[y][x].player)
-    },
-    setAccessibleCellsAroundPlayer(x, y, distance = 3) {
-      if(distance === 0) {
-        return
-      } else if(distance === 3) {
-        this.rows.forEach(row => {
-          const cells = row.filter(cell => { return cell.isAccessible })
-          cells.forEach(cell => {
-            cell.isAccessible = false
-          })
-        });
-      }
-      const directions = [
-        { x: 0, y: 1, label: 'right' }, { x: 0, y: -1, label: 'left' },
-        { x: 1, y: 0, label: 'up' }, { x: -1, y: 0, label: 'down' }
-      ]
-      directions.forEach(direction => {
-        const target = { x: x + direction.x, y: y + direction.y }
-        if(this.isWalkableCell(target)) {
-          this.rows[target.y][target.x].isAccessible = true
-          this.setAccessibleCellsAroundPlayer(target.x, target.y, distance - 1)
+    methods: {
+        setupObstacles(obstacleProbability) {
+            this.rows.map(row => {
+                row.map(cell => {
+                    const isObstacle = Math.random() < obstacleProbability
+                    if (isObstacle) {
+                        //console.log(cell)
+                    }
+                    if (cell.obstacleTile !== -1) {
+                        //cell.backgroundTile = cell.obstacleTile
+                    }
+                    //cell.obstacleTile = isObstacle ? 285 : -1
+                })
+            })
+        },
+        createGridFromJSON(data) {
+            console.log(data);
+            const width = data.width; // Map width
+            let currentRow = []; // Contains our future cells
+            for (let i = 0; i < data.layers[0].data.length; i++) {
+                if (i % width === 0 && i !== 0) {
+                    this.rows.push(currentRow);
+                    currentRow = [];
+                }
+                // Push our cell inside our current row
+                currentRow.push({
+                    x: i % width,
+                    y: Math.floor(i / width),
+                    backgroundTile: data.layers[0].data[i] - 1,
+                    obstacleTile: data.layers[1].data[i] - 1,
+                    decorationTile: data.layers[2].data[i] - 1,
+                });
+            }
+            this.rows.push(currentRow)
+        },
+        setupCurrentPlayer() {
+            console.log(this.players)
+            this.setAccessibleCellsAroundPlayer(this.currentPlayer.coordinates.x, this.currentPlayer.coordinates.y)
+        },
+        placePlayersOnMap() {
+            this.players.map(player => {
+                this.rows[player.coordinates.y][player.coordinates.x].player = player
+            })
+        },
+        isWalkableCell({x, y}) {
+            if (x < 0 || x >= this.rows[0].length || y < 0 || y >= this.rows.length) {
+                return false
+            }
+            return ((this.rows[y][x].obstacleTile === -1 || this.rows[y][x].decorationTile !== -1) && !this.rows[y][x].player)
+        },
+        setAccessibleCellsAroundPlayer(x, y, distance = 3) {
+            if (distance === 0) {
+                return
+            } else if (distance === 3) {
+                this.rows.forEach(row => {
+                    const cells = row.filter(cell => {
+                        return cell.isAccessible
+                    })
+                    cells.forEach(cell => {
+                        cell.isAccessible = false
+                    })
+                });
+            }
+            const directions = [
+                {x: 0, y: 1, label: 'right'}, {x: 0, y: -1, label: 'left'},
+                {x: 1, y: 0, label: 'up'}, {x: -1, y: 0, label: 'down'}
+            ]
+            directions.forEach(direction => {
+                const target = {x: x + direction.x, y: y + direction.y}
+                if (this.isWalkableCell(target)) {
+                    this.rows[target.y][target.x].isAccessible = true
+                    this.setAccessibleCellsAroundPlayer(target.x, target.y, distance - 1)
+                }
+            });
+        },
+        playerMove(arrival = {x: null, y: null}) {
+            console.log(this.currentPlayer, arrival)
+            this.$emit('updatePlayer', {player: this.currentPlayer, arrival: arrival})
+            this.rows[this.currentPlayer.coordinates.y][this.currentPlayer.coordinates.x].player = null
+            this.rows[arrival.y][arrival.x].player = this.currentPlayer
+            this.setAccessibleCellsAroundPlayer(arrival.x, arrival.y)
+            this.$forceUpdate()
         }
-      });
     },
-    playerMove(arrival = { x: null, y: null }) {
-      console.log(this.currentPlayer, arrival)
-      this.$emit('updatePlayer', { player: this.currentPlayer, arrival: arrival })
-      this.rows[this.currentPlayer.coordinates.y][this.currentPlayer.coordinates.x].player = null
-      this.rows[arrival.y][arrival.x].player = this.currentPlayer
-      this.setAccessibleCellsAroundPlayer(arrival.x, arrival.y)
-      this.$forceUpdate()
+    mounted() {
+        this.createGridFromJSON(dataMap)
+        this.placePlayersOnMap()
+        //this.setupObstacles(0.1)
+        this.setupCurrentPlayer()
+    },
+    watch: {
+        currentPlayer() {
+            this.setupCurrentPlayer()
+        }
     }
-  },
-  mounted() {
-    this.createGridFromJSON(dataMap)
-    this.placePlayersOnMap()
-    //this.setupObstacles(0.1)
-    this.setupCurrentPlayer()
-  },
-  watch: {
-    currentPlayer() {
-      this.setupCurrentPlayer()
-    }
-  }
 }
 </script>
 
 <style scoped>
 .grid-row {
-  display: flex;
-  flex-direction: row;
+    display: flex;
+    flex-direction: row;
 }
 
 .grid-container {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 .grid {
-  min-width: 650px;
+    min-width: 650px;
 }
 </style>
